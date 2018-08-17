35f2cf5e36b854069701dc037d618ce3d29a9398
Rakesh Kavitake <rakesh.kavitake@gmail.com>
2018-08-17 23:10:23 +0530

    First commit

---
+++ src/TestSingleton.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Rashmi on 02-01-2016.
+ */
+public class TestSingleton {
+    private static TestSingleton ourInstance = new TestSingleton();
+
+    public static TestSingleton getInstance() {
+        return ourInstance;
+    }
+
+    private TestSingleton() {
+    }
+}
